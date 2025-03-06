f6279c5db0b559b168d586ca490fd20c11700d66
Muzadded Chowdhury <alifchowdhury27@gmail.com>
2025-03-06 08:09:13 +0600

    Add Home Page and NavBar

---
+++ HandsOn_frontend/src/Pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+import NavBar from './components/NavBar';
+
+export default function HomePage() {
+  return (
+    <>
+       <NavBar />
+
+    </>
+  );
+}
