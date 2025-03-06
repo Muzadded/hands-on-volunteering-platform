14b4a696d5eb1b4198e3ff9cf64af81e2763896e
Muzadded Chowdhury <alifchowdhury27@gmail.com>
2025-03-06 09:01:26 +0600

    design Login Ui

--- HandsOn_frontend/src/routes/AppRoute.jsx
+++ HandsOn_frontend/src/routes/AppRoute.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../Pages/HomePage";
+import Login from "../Pages/login";
 
 
 const AppRoute = () => {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<Login />} />
         </Routes>
     )
 }
